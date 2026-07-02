0fcf52a02c6d0e4b7dda488cc5738e9d59022ed8
MiloradCvjetkovic <milorad.cvjetkovic@arm.com>
2026-07-02 10:40:10 +0200

    Update board layer for Alif AppKit-E7-AIML board to DFP v2.2.0

--- Alif/AppKit-E7_USB/Board/AppKit-E7_M55_HP/main.c
+++ Alif/AppKit-E7_USB/Board/AppKit-E7_M55_HP/main.c
@@ -139,15 +139,6 @@ void vbat_init(void)
         ~(VBAT_PWR_CTRL_TX_DPHY_ISO | VBAT_PWR_CTRL_RX_DPHY_ISO | VBAT_PWR_CTRL_DPHY_PLL_ISO);
 }
 
-static void CpuCacheEnable(void)
-{
-    /* Enable I-Cache */
-    SCB_EnableICache();
-
-    /* Enable D-Cache */
-    SCB_EnableDCache();
-}
-
 int main(void)
 {
 
@@ -177,9 +168,6 @@ int main(void)
 #ifdef CMSIS_shield_header
     shield_setup();
 #endif
-
-    /* Enable the CPU Cache */
-    CpuCacheEnable();
 
     return app_main();
 }
